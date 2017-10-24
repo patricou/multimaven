6bc8d5baab8e5cbaf767e8cdbe87488e0d170c08
Patricou <deschamps.pat@gmail.com>
2017-10-24 19:00:18 +0200

    Commit 1.0.1-SNAPSHOT

--- multimaven-web/src/main/java/com/pat/multipat/HelloServlet.java
+++ multimaven-web/src/main/java/com/pat/multipat/HelloServlet.java
@@ -5,6 +5,9 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 
 /**
  * Created by patricou on 22/10/2017.
@@ -12,11 +15,18 @@ import java.io.PrintWriter;
 public class HelloServlet
     extends javax.servlet.http.HttpServlet
 implements javax.servlet.Servlet {
+        private static final Logger log = LoggerFactory.getLogger(HelloServlet.class);
+        @Override
         protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
             response.setContentType("text/html");
-            PrintWriter out = response.getWriter();
-            out.println("Projet Maven la centrale-web : Servlet Lancée");
-            out.flush();
-            out.close();
+            try {
+                log.info("hi");
+                PrintWriter out = response.getWriter();
+                out.println("Projet Maven la centrale-web : Servlet Lancée");
+                out.flush();
+                out.close();
+            }catch (IOException e){
+                log.info("Exception in Servlet");
+            }
         }
     }
